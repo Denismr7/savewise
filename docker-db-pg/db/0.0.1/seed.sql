106792c93a76232fbfa89a913135ca89f65c3722
Denis Mory <denismr7@gmail.com>
2020-12-12 15:55:58 +0100

    Created table TransactionTypes

--- docker-db-pg/db/0.0.1/seed.sql
+++ docker-db-pg/db/0.0.1/seed.sql
@@ -1,1 +1,5 @@
-INSERT INTO Users (u_id, u_login, u_name, u_lastname, u_password) VALUES (1, N'Admin', N'Administrator', N'Savewise', N'2911')
+INSERT INTO Users (u_id, u_login, u_name, u_lastname, u_password) VALUES (1, N'Admin', N'Administrator', N'Savewise', N'2911');
+
+INSERT INTO TransactionTypes (tt_id, tt_name) VALUES (1, 'Ingreso');
+INSERT INTO TransactionTypes (tt_id, tt_name) VALUES (2, 'Gasto');
+INSERT INTO TransactionTypes (tt_id, tt_name) VALUES (3, 'Inversión');
